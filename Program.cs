0d5fb91fbcb5992c92e175b83f5a5c142909469b
Davi Borges <davifelipe_300@hotmail.com>
2022-02-11 11:14:47 -0300

    Arquivos aula operador logico not (!)

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ExemploOperadorLogicoNot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*##### VARIÁVEIS #####*/
+            bool resultado;
+            /*##### VARIÁVEIS #####*/
+
+            resultado = !(7 <= 4);
+            //aqui o operador logico not(!) vai inverter o resultado da expressão que por questão de precedencia tem que ficar dentro de parenteses para ser executada primeiramente, sendo que esta verificando se sete é igual ou menor(<=) que quatro, no caso o valor desta expressão é false e, foi invertida para "true" pelo operador logico not (!).
+
+            Console.WriteLine(resultado);
+        }
+    }
+}
